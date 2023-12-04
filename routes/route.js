358803a079d30d937be2887eba2682073d167657
PRAVEENTH R <88272102+praveenthravichandran@users.noreply.github.com>
2023-12-04 21:06:33 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,11 @@
 import express from  'express';
 import { getProductById, getProducts } from '../controller/product-controller.js';
 import { userSignUp, userLogIn } from '../controller/user-controller.js';
-// import { addItemInCart } from '../controller/cart-controller.js';
-import { addPaymentGateway, paymentResponse } from '../controller/payment-controller.js';
+import { addItemInCart } from '../controller/cart-controller.js';
+import { createOrder } from '../controller/payment-controller.js';
+import { payOrder } from '../controller/payment-controller.js';
+import { paymentResponse } from '../controller/payment-controller.js';
+
 
 const router = express.Router();
 
@@ -13,9 +16,14 @@ router.post('/login', userLogIn);
 router.get('/products', getProducts);
 router.get('/product/:id', getProductById);
 
-// router.post('/cart/add', addItemInCart);
+router.post('/cart/add', addItemInCart);
 
-router.post('/payment', addPaymentGateway);
-router.post('/callback', paymentResponse);
+router.get('/get-razorpay-key', (req, res) => {
+    res.send({ key: process.env.RAZORPAY_KEY_ID });
+  });
+  
+router.post("/create-order", createOrder);
+router.post('/pay-order', payOrder);
+router.get('/pay-res', paymentResponse);
 
 export default router;
